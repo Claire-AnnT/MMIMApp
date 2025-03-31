f34f8b4eeb6c9b042de835f9580d0a95e8cd4548
Claire-AnnT <claireann.telemaque@gmail.com>
2025-03-31 16:52:06 -0400

    Add project files.

---
+++ Data/MMIMAppContext.cs
@@ -0,0 +1,18 @@
+﻿using MMIMApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MMIMApp.Data
+{
+    class MMIMAppContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; } = null!;
+        public DbSet<Product> Products { get; set; } = null!;
+        public DbSet<Report> Reports { get; set; } = null!;
+        public DbSet<User> Users { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=localdb.db; Foreign Keys=True; MaxPoolSize=16;");
+        }
+    }
+}
