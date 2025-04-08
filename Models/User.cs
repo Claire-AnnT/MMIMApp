1a389e986b1cd0f15effe6048f80fea110485b0d
Claire-AnnT <claireann.telemaque@gmail.com>
2025-04-07 21:53:21 -0400

    Scaffolded user model

--- Models/User.cs
+++ Models/User.cs
@@ -8,5 +8,11 @@ namespace MMIMApp.Models
 {
     class User
     {
+        public uint Id { get; set; }
+        public string Username { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public bool IsAdmin { get; set; } = false;
     }
 }
