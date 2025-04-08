4f1c90d4e86b5d89069d05ef97f40ef51c8871c9
Claire-AnnT <claireann.telemaque@gmail.com>
2025-04-07 21:37:47 -0400

    Scaffolded product model.

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,17 @@ namespace MMIMApp.Models
 {
     class Product
     {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Brand { get; set; } = null!;
+        public string? Description { get; set; }
+        
+        [Column (TypeName = "decimal(18,2)")]
+        public decimal UnitPrice { get; set; }
+        public int Unit { get; set; }
+        public int MinUnit { get; set; }
+        
+        public User User { get; set; } = null!;
+        public Category? Category { get; set; }
     }
 }
