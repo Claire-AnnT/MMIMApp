e5f33ec6cd91ba45ac83b5a30249beb36b30baaa
Claire-AnnT <claireann.telemaque@gmail.com>
2025-04-14 18:01:19 -0400

    Modified product model.

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
@@ -9,23 +10,28 @@ namespace MMIMApp.Models
 {
     class Product
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; } = null!;
+        [Required]
         public string Brand { get; set; } = null!;
         public string? Description { get; set; }
-        
-        [Column (TypeName = "decimal(18,2)")]
+
+        [Column(TypeName = "decimal(18, 2)"),Required]
         public decimal UnitPrice { get; set; }
+        [Required]
         public int Unit { get; set; }
         public int MinUnit { get; set; }
-        
+        [Required]
         public uint CreatedByUserId { get; set; }
+        [ForeignKey(nameof(CreatedByUserId))]
         public User CreatedByUser { get; set; } = null!;
-
-        public ushort CategoryId { get; set; }
+        public ushort? CategoryId { get; set; }
+        [ForeignKey(nameof(CategoryId))]
         public Category? Category { get; set; }
 
-        public Product(int id, string name, string brand, decimal unitPrice, int unit, int minUnit, uint createdByUserId, User createdByUser)
+        public Product(int id, string name, string brand, decimal unitPrice, int unit, int minUnit, User createdByUser, Category? category=null)
         {
             Id = id;
             Name = name;
@@ -33,67 +39,12 @@ namespace MMIMApp.Models
             UnitPrice = unitPrice;
             Unit = unit;
             MinUnit = minUnit;
-            CreatedByUserId = createdByUserId;
+            CreatedByUserId = createdByUser.Id;
             CreatedByUser = createdByUser;
-        }
-
-        public void setCategory(ushort categoryId, Category category)
-        {
-            if (categoryId == 0)
-            {
-                throw new ArgumentException("Category ID cannot be 0");
-            }
-            if (category == null)
-            {
-                throw new ArgumentNullException(nameof(category), "Category cannot be null");
-            }
-            
-            CategoryId = categoryId;
             Category = category;
+            CategoryId = category?.Id;
         }
 
-        public void addStock(int amount)
-        {
-            if (amount <= 0)
-            {
-                throw new ArgumentException("Amount must be greater than 0");
-            }
-            Unit += amount;
-        }
-
-        public void removeStock(int amount)
-        {
-            if (amount <= 0)
-            {
-                throw new ArgumentException("Amount must be greater than 0");
-            }
-            Unit -= amount;
-            if (Unit < MinUnit)
-            {
-                lowStockWarning();
-            }
-            else if (Unit <= 0)
-            {
-                emptyStockWarning();
-            }
-        }
-
-        public string lowStockWarning()
-        {
-            if (Unit < MinUnit)
-            {
-                return $"Warning: {Name} is low on stock. Only {Unit} units left.";
-            }
-            return $"{Name} is in stock.";
-        }
-
-        public string emptyStockWarning()
-        {
-            if (Unit <= 0)
-            {
-                return $"Warning: {Name} has no more stock.";
-            }
-            return $"{Name} is in stock.";
-        }
+        
     }
 }
